3fd6f2e09b5c38b06c7b40af6557b86621a5eeb1
curlos <curlosmart@gmail.com>
2020-09-18 11:55:25 -0400

    ..

--- 01-part3-notes-backend/index.js
+++ 01-part3-notes-backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -43,8 +45,8 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -77,7 +79,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
